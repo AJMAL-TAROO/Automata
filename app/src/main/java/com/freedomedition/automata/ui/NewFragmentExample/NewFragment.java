aeabf8c53033247a28752318d55ec7367b77b1ca
DESKTOP-IPQAFDN\hp <ajtaroo@gmail.com>
2024-05-05 22:09:36 +0400

    plant animation working

--- app/src/main/java/com/freedomedition/automata/ui/NewFragmentExample/NewFragment.java
+++ app/src/main/java/com/freedomedition/automata/ui/NewFragmentExample/NewFragment.java
@@ -1,22 +1,15 @@
 package com.freedomedition.automata.ui.NewFragmentExample;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
-import android.webkit.WebViewClient;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
 import com.freedomedition.automata.R;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -28,32 +21,23 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
 
 public class NewFragment extends Fragment {
-
     private NewViewModel newViewModel;
-
     DatabaseReference dref;
-
-
     TextView temperatureLoadingtxt;
     TextView humidityLoadingtxt;
     TextView tdsLoadingtxt;
-
     String str_temperatureLoading;
     String str_humidityLoading;
     String str_tdsLoading;
-
     ProgressBar progressbarTemperature;
     ProgressBar progressbarHumidity;
-
-
+    ImageView imageView;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         newViewModel =
                 ViewModelProviders.of(this).get(NewViewModel.class);
-
         View root = inflater.inflate(R.layout.new_layout, container, false);
-
 
         humidityLoadingtxt = root.findViewById(R.id.humidityLoadingtxt);
         temperatureLoadingtxt = root.findViewById(R.id.temperatureLoadingtxt);
@@ -61,10 +45,11 @@ public class NewFragment extends Fragment {
 
         progressbarHumidity = root.findViewById(R.id.progressHumidity);
         progressbarTemperature = root.findViewById(R.id.progressTemperature);
+        imageView = root.findViewById(R.id.plantImageView);
 
+        Glide.with(this).load(R.drawable.plant_animation).into(imageView);
         progressbarTemperature.setMax(100);
         progressbarHumidity.setMax(100);
-
 
         dref= FirebaseDatabase.getInstance().getReference().child("TEMPERATURE");
         dref.addValueEventListener(new ValueEventListener() {
@@ -78,7 +63,6 @@ public class NewFragment extends Fragment {
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
 
@@ -92,7 +76,6 @@ public class NewFragment extends Fragment {
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
 
@@ -107,15 +90,12 @@ public class NewFragment extends Fragment {
             }
             @Override
             public void onCancelled(DatabaseError databaseError) {
-
             }
         });
 
-
-
-
         return root;
     }
+
 
 
 }
