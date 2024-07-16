6ac9d535ad8637830b42102f5bcbc49545994a49
DESKTOP-IPQAFDN\hp <ajtaroo@gmail.com>
2024-07-16 18:31:40 +0400

    Modules Fragment - Google Maps shifted in a pop up dialog successfully for DHT only

---
+++ app/src/main/java/com/freedomedition/automata/ui/CustomDialog.java
@@ -0,0 +1,11 @@
+package com.freedomedition.automata.ui;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AlertDialog;
+
+public class CustomDialog extends AlertDialog {
+    public CustomDialog(Context context){
+        super(context);
+    }
+}
--- app/src/main/java/com/freedomedition/automata/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/freedomedition/automata/ui/notifications/NotificationsFragment.java
@@ -2,6 +2,8 @@ package com.freedomedition.automata.ui.notifications;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,18 +17,27 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.bumptech.glide.Glide;
 import com.freedomedition.automata.R;
+import com.freedomedition.automata.ui.CustomDialog;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-public class NotificationsFragment extends Fragment {
+public class NotificationsFragment extends Fragment implements OnMapReadyCallback{
     private NotificationsViewModel notificationsViewModel;
     ImageView imageView; ImageView dht11image; ImageView tdsimage;
     TextView dht11txt; TextView tdstxt;
     DatabaseReference dref;
     String string_dht11;
     String string_tds;
+
+    GoogleMap mMap;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -85,7 +96,7 @@ public class NotificationsFragment extends Fragment {
                 details_dht11 = "Sensor might not be connected. \nPlease check the wiring connection.";
             }
             AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-            builder.setCancelable(false);
+            builder.setCancelable(true);
             builder.setTitle("DHT11");
             builder.setMessage(details_dht11);
             if(string_dht11.equals("ON")) {
@@ -94,6 +105,7 @@ public class NotificationsFragment extends Fragment {
             }
             if(string_dht11.equals("OFF")){
                 builder.setNegativeButton("Take Action",(dialog, which) -> {
+                    getDHTAction();
                 });
             }
             builder.create().show();
@@ -107,7 +119,7 @@ public class NotificationsFragment extends Fragment {
                 details_tds = "Sensor might not be connected. \n1. Check the wiring connection. \n2. If sensor is in water.";
             }
             AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
-            builder.setCancelable(false);
+            builder.setCancelable(true);
             builder.setTitle("TDS");
             builder.setMessage(details_tds);
             if(string_tds.equals("ON")) {
@@ -123,4 +135,55 @@ public class NotificationsFragment extends Fragment {
 
         return root;
     }
+
+    public void getDHTAction(){
+        String actions_tds = "You can either: \n1. Crosscheck the wiring connection provided\n2. Buy a new component at the shops provided";
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setCancelable(true);
+        builder.setTitle("Actions that can be taken");
+        builder.setMessage(actions_tds);
+        builder.setPositiveButton("Check wiring", (dialog, which) -> {
+            checkDHTConnection();
+        });
+        builder.setNegativeButton("Buy locally",(dialog, which) -> {
+            final CustomDialog dialog1 = new CustomDialog(getContext());
+            View view = getLayoutInflater().inflate(R.layout.popup_donation, null);
+            dialog1.setView(view);
+            dialog1.setCancelable(true);
+
+
+            // Get the SupportMapFragment from the inflated layout
+            SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+            mapFragment.getMapAsync(this);
+
+            dialog1.show();
+
+//            listView = view.findViewById(R.id.listView);
+//            labelDonation = view.findViewById(R.id.labelDonation);
+        });
+        builder.create().show();
+    }
+
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker at the specified location
+        LatLng location = new LatLng(-20.25578199000835, 57.481395786625434);
+        mMap.addMarker(new MarkerOptions().position(location).title("Marker at specified coordinates"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(location));
+    }
+    public void checkDHTConnection(){
+        String connectionDetails;
+        connectionDetails = "1. Connect GND from DHT11 to GND in NodeMCU\n2. Connect VCC from DHT11 to 3V in NodeMCU\n 3. Connect Data from DHT11 to D4 in NodeMCU";
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setCancelable(true);
+        builder.setTitle("DHT wiring connection");
+        builder.setMessage(connectionDetails);
+        builder.setPositiveButton("OK", (dialog, which) -> {
+        });
+//        builder.setNegativeButton("Buy locally",(dialog, which) -> {
+//        });
+        builder.create().show();
+    }
+
 }
